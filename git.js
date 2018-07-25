6370b75427a2eb2d8f1ff6dd0692ec4a5fee8d49
Yossi <yossiytd@gmail.com>
2018-07-25 18:08:46 +0300

    setup script and heroku port

---
+++ git.js
@@ -0,0 +1,40 @@
+//git status - tyo know what is the status of the tracking
+
+//git add to track after file or folder git add (some file/dir)
+
+//we can create .gitignore file and ignore tracking from files and folders from file
+
+//need to update that file to track from command git add .gitignore
+
+//create commit git commit -m(message) first commit
+
+//check if generated ssh key ls -al ~/.ssh
+
+//to generate new ssh  ssh-keygen -t rsa -b 4096 -C 'yossityd@gmail.com'
+
+//add the key  eval "$(ssh-agent -s)" start to ssh engine program or let us the ssh id
+
+//tell to ssh engeine to know where the file ssh-add ~/.ssh/id_rsa
+
+//adding ssh key to github from terminal clip < ~/.ssh/id_rsa.pub and paste to ssh key on git hub
+
+//test to connection from terminal ssh -T git@github.com
+
+//to push our code to github or server we need to do that git remote add origin https://github.com/jordanytd/webserver.git
+//need to change the url if we need to add the repo to another server
+//git push -u origin master
+
+//update ssh key on eroku heroku keys:add
+//check if keys up heroku keys
+
+//remove keys  heroku keys:remove email - email of this key
+
+//check if all ready ssh -v git@heroku.com
+
+//need to define some properties on server file like port dynamic
+
+
+
+
+
+
